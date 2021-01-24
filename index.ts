75b1cb9c940258554c0bf49bba70159349b0d2b5
Federico Brigante <me@fregante.com>
2021-01-23 22:09:50 -0600

    A * protocol now includes ws/wss in Firefox
    Fixes #1

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // Copied from https://github.com/mozilla/gecko-dev/blob/073cc24f53d0cf31403121d768812146e597cc9d/toolkit/components/extensions/schemas/manifest.json#L487-L491
 export const patternValidationRegex = /^(https?|wss?|file|ftp|\*):\/\/(\*|\*\.[^*/]+|[^*/]+)\/.*$|^file:\/\/\/.*$|^resource:\/\/(\*|\*\.[^*/]+|[^*/]+)\/.*$|^about:/;
 
+const isFirefox = typeof navigator === 'object' && navigator.userAgent.includes('Firefox/');
+
 function getRawRegex(matchPattern: string): string {
 	if (!patternValidationRegex.test(matchPattern)) {
 		throw new Error(matchPattern + ' is an invalid pattern, it must match ' + String(patternValidationRegex));
@@ -9,7 +11,7 @@ function getRawRegex(matchPattern: string): string {
 	let [, protocol, host, pathname] = matchPattern.split(/(^[^:]+:[/][/])([^/]+)?/);
 
 	protocol = protocol
-		.replace('*', 'https?') // Protocol wildcard
+		.replace('*', isFirefox ? '(https?|wss?)' : 'https?') // Protocol wildcard
 		.replace(/[/]/g, '[/]'); // Escape slashes
 
 	host = (host ?? '') // Undefined for file:///
