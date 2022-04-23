2c7884d3e453745bee37f96677e84c94b1d7a01e
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2022-04-23 19:03:13 +0200

    add exclude test

--- globs.test.js
+++ globs.test.js
@@ -1,31 +1,50 @@
 import test from 'ava';
 import {globToRegex} from './index.js';
 
-function macro(t, glob, matching) {
+function testMatch(t, glob, matching) {
 	const regex = globToRegex(glob);
 	for (const url of matching) {
 		t.regex(url, regex);
 	}
 }
 
-macro.title = (_, glob) => glob;
+testMatch.title = (_, glob) => glob;
 
 // Pulled from https://developer.chrome.com/docs/extensions/mv3/content_scripts/#matchAndGlob
-const map = new Map();
-map.set('https://???.example.com/foo/*', [
+const shouldMatch = new Map();
+shouldMatch.set('https://???.example.com/foo/*', [
 	'https://www.example.com/foo/bar',
 	'https://the.example.com/foo/',
 ]);
 
-map.set('*nytimes.com/???s/*', [
+shouldMatch.set('*nytimes.com/???s/*', [
 	'https://www.nytimes.com/arts/index.html',
 	'https://www.nytimes.com/jobs/index.html',
 ]);
 
-map.set('*google.com*', [
+shouldMatch.set('*google.com*', [
 	'https://google.com/',
 ]);
 
-for (const [glob, urls] of map) {
-	test(macro, glob, urls);
+for (const [glob, urls] of shouldMatch) {
+	test(testMatch, glob, urls);
+}
+
+function testExclude(t, glob, matching) {
+	const regex = globToRegex(glob);
+	for (const url of matching) {
+		t.notRegex(url, regex);
+	}
+}
+
+testExclude.title = (_, glob) => glob;
+
+const shouldExclude = new Map();
+
+shouldExclude.set('*bar*', [
+	'http://www.google.com/foo',
+]);
+
+for (const [glob, urls] of shouldExclude) {
+	test(testExclude, glob, urls);
 }
