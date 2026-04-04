58ae7b24a946ea58cf9c00f91e09902877c066c4
fregante <me@fregante.com>
2026-04-04 13:34:50 +0700

    Discard changes to test/duplicate-patterns.js

--- test/duplicate-patterns.js
+++ test/duplicate-patterns.js
@@ -1,16 +1,17 @@
-import {test, expect} from 'vitest';
+import test from 'ava';
 import {removeRedundantPatterns} from '../index.js';
 
-test('removeRedundantPatterns', () => {
-	expect(
+test('removeRedundantPatterns', t => {
+	t.deepEqual(
 		removeRedundantPatterns([
 			'https://*.example.com/*',
 			'https://*.example.com/*',
 		]),
+		['https://*.example.com/*'],
 		'identical patterns should be detected',
-	).toEqual(['https://*.example.com/*']);
+	);
 
-	expect(
+	t.deepEqual(
 		removeRedundantPatterns([
 			'http://neverssl.com/*',
 			'https://*.example.com/*',
@@ -18,44 +19,49 @@ test('removeRedundantPatterns', () => {
 			'https://fregante.com/*',
 			'*://*/*',
 		]),
+		['<all_urls>'],
 		'<all_urls> should catch all',
-	).toEqual(['<all_urls>']);
+	);
 
-	expect(
+	t.deepEqual(
 		removeRedundantPatterns([
 			'http://neverssl.com/*',
 			'https://*.example.com/*',
 			'*://*/*',
 			'https://fregante.com/*',
 		]),
+		['*://*/*'],
 		'*://*/* should catch all',
-	).toEqual(['*://*/*']);
+	);
 
-	expect(
+	t.deepEqual(
 		removeRedundantPatterns([
 			'http://*.example.com/*',
 			'https://*/*',
 			'https://fregante.com/*',
 		]),
+		['http://*.example.com/*', 'https://*/*'],
 		'https://*/* should drop all other https origins',
-	).toEqual(['http://*.example.com/*', 'https://*/*']);
+	);
 
-	expect(
+	t.deepEqual(
 		removeRedundantPatterns([
 			'https://git.example.com/*',
 			'https://*.example.com/*',
 			'https://example.com/*',
 			'https://fregante.com/*',
 		]),
+		['https://*.example.com/*', 'https://fregante.com/*'],
 		'A subdomain star should drop all other same-domain origins',
-	).toEqual(['https://*.example.com/*', 'https://fregante.com/*']);
+	);
 
-	expect(
+	t.deepEqual(
 		removeRedundantPatterns([
 			'https://git.example.com/*',
 			'https://git.example.com/fregante/*',
 		]),
+		['https://git.example.com/*'],
 		'A pathname star should drop all other same-origin origins',
-	).toEqual(['https://git.example.com/*']);
+	);
 });
 
