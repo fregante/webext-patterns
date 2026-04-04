40d6b98245adfcf6c394f9124cd36803109487ee
fregante <me@fregante.com>
2026-04-04 13:13:47 +0700

    Discard changes to test/globs.js

--- test/globs.js
+++ test/globs.js
@@ -18,7 +18,8 @@ function testExclude(t, glob, matching) {
 testMatch.title = (_, glob, url) => `${glob} should match ${url}`;
 testExclude.title = (_, glob, url) => `${glob} should not match ${url}`;
 
-const globs = new Map([['https://???.example.com/foo/*', [[
+const globs = new Map();
+globs.set('https://???.example.com/foo/*', [[
 	// Matching
 	'https://www.example.com/foo/bar',
 	'https://the.example.com/foo/',
@@ -26,31 +27,41 @@ const globs = new Map([['https://???.example.com/foo/*', [[
 ], [
 	// Not matching
 	'https://t.e.example.com/foo',
-]]], ['*props[]=a&props[]=b', [[
+]]);
+
+globs.set('*props[]=a&props[]=b', [[
 	'https://example.com/?props[]=a&props[]=b',
 ], [
 	'https://example.com/?props[]=b&props[]=a',
-]]], ['*nytimes.com/???s/*', [[
+]]);
+
+globs.set('*nytimes.com/???s/*', [[
 	'https://www.nytimes.com/arts/index.html',
 	'https://www.nytimes.com/jobs/index.html',
 ], [
 	'https://www.nytimes.com/s123/index.html',
-]]], ['*go*ogle.com*', [[
+]]);
+
+globs.set('*go*ogle.com*', [[
 	'https://google.com/',
 	'https://go123ogle.com/',
 ], [
 	'https://gougle.com/',
-]]], ['*go???ogle.com*', [[
+]]);
+
+globs.set('*go???ogle.com*', [[
 	'https://go123ogle.com/',
 	'https://go12ogle.com/',
 	'https://google.com/',
 ], [
 	'https://go1234ogle.com/',
-]]], ['*bar*', [[
+]]);
+
+globs.set('*bar*', [[
 	'http://www.google.com/bar',
 ], [
 	'http://www.google.com/foo',
-]]]]);
+]]);
 
 for (const [glob, [shouldMatchUrls, shouldNotMatchUrls]] of globs) {
 	test(testMatch, glob, shouldMatchUrls);
